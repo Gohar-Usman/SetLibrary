f3413fff3e64041ab20366f58476ebe6416b2d02
gohar <goharusman1999@gmail.com>
2021-12-09 17:28:24 +0500

    old version

--- app/src/main/java/jav/app/setlibrary/MainActivity.java
+++ app/src/main/java/jav/app/setlibrary/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.toaster.ToastMessageLastTry;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToastMessageLastTry.s(this,"cccccccccccccccccccccccccccccccccccc");
         Toast.makeText(this, "dddddd", Toast.LENGTH_SHORT).show();
     }
 }
