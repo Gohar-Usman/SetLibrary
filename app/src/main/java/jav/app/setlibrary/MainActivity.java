7f96bd0173ba78fdc1d14c88f58e529525af78b0
gohar <goharusman1999@gmail.com>
2021-12-09 17:39:42 +0500

    old version2

--- app/src/main/java/jav/app/setlibrary/MainActivity.java
+++ app/src/main/java/jav/app/setlibrary/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.example.toaster.ToastMessageLastTry;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToastMessageLastTry.s(this,"cccccccccccccccccccccccccccccccccccc");
         Toast.makeText(this, "dddddd", Toast.LENGTH_SHORT).show();
     }
 }
